7b73999f2bfc011c99d19f5013ef245f879dbed8
RYAN-DESKTOP\Ryan <ryanjamespowell13@gmail.com>
2021-12-13 23:21:45 -0600

    added id to each quote object and added route to each id

--- index.js
+++ index.js
@@ -11,25 +11,36 @@ const quotes = []
 
 
 app.get('/marx', (req, res) => {
-    axios.get('https://www.marxists.org/archive/marx/works/subject/quotes/index.htm')
-        .then((response) => {
-            const html = response.data
-            const $ = cheerio.load(html)
+  axios.get('https://www.marxists.org/archive/marx/works/subject/quotes/index.htm')
+      .then((response) => {
+        const html = response.data
+        const $ = cheerio.load(html)
+        let id = 1
 
-            $('p:contains("Marx")', html).each(function () {
-                const quoteSplit = $(this).text().split("\n")
-                const quote = quoteSplit[0]
-                const source = quoteSplit[1]
+          $('p:contains("Marx")', html).each(function () {
+            const quoteSplit = $(this).text().split("\n")
+            const quote = quoteSplit[0]
+            const source = quoteSplit[1]
 
-                if (quote != "" && source != null && source.includes("Marx"))
-                quotes.push({
-                    quote,
-                    source
-                })
+            
+            if (quote != "" && source != null && source.includes("Marx")){
+            quotes.push({
+                id,
+                quote,
+                source
             })
-            res.json(quotes)
-        })
+            id++
+          }
+          })
+        res.json(quotes)
+      })
         
+})
+
+app.get('/marx/:id', (req, res) => {
+  let specificQuote = quotes.find(c => c.id === parseInt(req.params.id))
+  if (!specificQuote) res.status(404).send("The quote with the provided ID does not exist")
+  res.send(specificQuote);
 })
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`))
